3b232c0ff93fc7663a848b4bc482ed6ea091c285
AlexandrVainer <alexva743@gmail.com>
2022-04-24 19:19:57 +0300

    init

---
+++ FactoryPractice/DietFactory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class DietFactory
+    {
+        public static DietBase GetDiet(string id)
+        {
+            if (id == "vn")
+                return new VeganDiet();
+            if (id == "vt")
+                return new VegetarianDiet();
+            if (id == "lf")
+                return new LightFood();
+            return null;
+        }
+    }
+
+    public abstract class DietBase
+    {
+        public abstract void PrintAllowedItems();
+    }
+    public class VeganDiet : DietBase
+    {
+        public override void PrintAllowedItems()
+        {
+            foreach (var item in "Fruits,Grains,Vegetables".Split(','))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public class VegetarianDiet : VeganDiet
+    {
+        public override void PrintAllowedItems()
+        {
+            base.PrintAllowedItems();
+            foreach (var item in "Eggs,Dairy products".Split(','))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class LightFood : VegetarianDiet
+    {
+        public override void PrintAllowedItems()
+        {
+            base.PrintAllowedItems();
+            foreach (var item in "Meat,Fish".Split(','))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
---
+++ FactoryPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using Factory;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine(@"Enter the diet : 
+        vn : Vegan diet
+        vt : Vegetarian diet
+        lf : Light food
+        q : quit
+        ");
+    string input = Console.ReadLine();
+    if (input == "q")
+        break;
+    DietBase diet = DietFactory.GetDiet(input);
+    if (diet != null)
+        diet.PrintAllowedItems();
+    else
+        Console.WriteLine("Unknow diet name");
+    Console.WriteLine("Enter any key to recieve another diet menu");
+    Console.ReadLine();
+
+} while (true);
